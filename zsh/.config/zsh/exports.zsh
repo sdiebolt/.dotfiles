0acd3dcab66e934bf3febbd46e59a671701933a5
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-07-18 14:17:11 +0200

    feat: add nvm configuration to zshrc

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -26,3 +26,8 @@ export FZF_DEFAULT_COMMAND='fdfind --type f --strip-cwd-prefix --hidden --follow
 
 # Python virtual environments
 export VIRTUALENVS="$HOME/.virtualenvs"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
