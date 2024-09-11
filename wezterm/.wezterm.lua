6b0b95170bf81018cb56d6fbcbc402cc19db1c74
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-09-11 22:21:49 +0200

    feat: replace kitty by wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,12 @@
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = "Dracula (Official)"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+-- and finally, return the configuration to wezterm
+return config
