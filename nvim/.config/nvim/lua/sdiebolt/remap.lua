538e49a31bfed2b2c3945b2bfeb296247a909c2f
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-10-25 12:44:29 +0200

    feat(nvim): add new key mappings and settings
    - Added key mappings for exiting terminal mode, moving lines, and more.
    - Improved cursor behavior for various actions.
    - Added clipboard support for yank and paste.
    - Enhanced quickfix navigation and interactive search/replace.
    - Added settings for cursorline, line numbering, and tab size.
    - Enabled termguicolors and set scrolloff, signcolumn, and more.
    - Configured split window positions and whitespace display.
    - Disabled editor mode display.

--- nvim/.config/nvim/lua/sdiebolt/remap.lua
+++ nvim/.config/nvim/lua/sdiebolt/remap.lua
@@ -1,34 +1,57 @@
 vim.g.mapleader = " "
 
+-- Exit terminal mode.
 vim.keymap.set("t", "<leader><ESC>", "<C-\\><C-n>")
+
+-- Move lines up and down in visual mode.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Keep cursor in place when joining lines.
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in place when moving up/down using C-d/C-u.
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in place when searching forward/backward.
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+-- Paste without replacing the register.
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- Delete without replacing the register.
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- Yank to clipboard.
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+-- Paste from clipboard.
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
 
+-- Avoid repetitions.
 vim.keymap.set("n", "Q", "<nop>")
+
+-- Format.
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- Move faster in quickfix.
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Interactive search and replace.
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "Replace interactively" })
+
+-- Set executable.
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- Diagnostic hover window
+-- Diagnostic hover window.
 vim.keymap.set("n", "<leader>dh", "<Cmd>lua vim.diagnostic.open_float()<CR>")
+
+-- Save file.
+vim.keymap.set("n", "<leader>w", ":w<CR>")
--- nvim/.config/nvim/lua/sdiebolt/set.lua
+++ nvim/.config/nvim/lua/sdiebolt/set.lua
@@ -1,36 +1,57 @@
 vim.opt.guicursor = ""
 
+-- Show which line your cursor is on.
+vim.opt.cursorline = true
+
+-- Line numbering.
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Set tab size.
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Smart autoindenting when starting a new line.
 vim.opt.smartindent = true
 
+-- Disable wrapping.
 vim.opt.wrap = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
+-- Set swap file
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
+-- Enable colors in the TUI.
 vim.opt.termguicolors = true
 
+-- Set minimum number of lines above and below cursor.
 vim.opt.scrolloff = 8
+
+-- Setup sign column.
 vim.opt.signcolumn = "yes"
+
+-- Include "@" in filenames.
 vim.opt.isfname:append("@-@")
 
+-- Update time from swap file to disk.
 vim.opt.updatetime = 50
 
+-- Color column and text width.
 vim.opt.colorcolumn = "89"
-
 vim.opt.textwidth = 88
+
+-- Split window positions.
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Do not show editor mode.
+vim.opt.showmode = false
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
