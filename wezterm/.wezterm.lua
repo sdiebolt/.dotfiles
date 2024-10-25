23943d096f5366dc6401fb85c0b886348fbee5a6
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-10-25 10:52:00 +0200

    feat: set font size to 10.0 in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,6 +7,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Dracula (Official)"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+config.font_size = 10.0
 
 -- and finally, return the configuration to wezterm
 return config
