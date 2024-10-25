dacabf791aaff4e824b66d5fe0a2dfbf9c9d7124
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-10-25 12:46:54 +0200

    feat(dap): enhance nvim-dap setup with UI and key mappings
    - Added dependencies for nvim-dap-ui and nvim-nio.
    - Configured key mappings for debugging actions.
    - Set up dap-python with virtual environment.
    - Defined custom signs for breakpoints and log points.
    - Integrated nvim-dap-ui to open/close with debug events.

--- nvim/.config/nvim/lua/sdiebolt/lazy/dap.lua
+++ nvim/.config/nvim/lua/sdiebolt/lazy/dap.lua
@@ -1,12 +1,46 @@
 return {
     {
         "mfussenegger/nvim-dap",
-    },
-    {
-        "mfussenegger/nvim-dap-python",
+
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+            "mfussenegger/nvim-dap-python"
+        },
+
+        keys = {
+            { "<leader>ds", function() require("dap").continue() end,          desc = "Continue/start debug" },
+            { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
+            { "<leader>dx", function() require("dap").terminate() end,         desc = "Terminate debug" },
+            { "<leader>do", function() require("dap").step_over() end,         desc = "Step over" },
+            { "<leader>di", function() require("dap").step_into() end,         desc = "Step into" },
+        },
 
         config = function()
             require("dap-python").setup(os.getenv("VIRTUALENVS") .. "/debugpy/bin/python")
+
+            -- Signs
+            for _, group in pairs({
+                "DapBreakpoint",
+                "DapBreakpointCondition",
+                "DapBreakpointRejected",
+                "DapLogPoint",
+            }) do
+                vim.fn.sign_define(group, { text = "●", texthl = group })
+            end
+
+            -- nvim-dap-ui
+            require("dapui").setup()
+            local dap, dapui = require("dap"), require("dapui")
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
         end,
     }
 }
