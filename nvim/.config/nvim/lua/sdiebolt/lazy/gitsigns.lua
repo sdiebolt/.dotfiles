1d08809a8a4b6e9184825368041c90f59e1f188e
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-10-25 12:47:34 +0200

    feat(gitsigns): add current line blame and key mappings
    - Enabled current line blame with customizable options.
    - Added key mapping for blaming the current line with full details.
    - Integrated gitsigns with scrollbar handlers.

--- nvim/.config/nvim/lua/sdiebolt/lazy/gitsigns.lua
+++ nvim/.config/nvim/lua/sdiebolt/lazy/gitsigns.lua
@@ -2,7 +2,30 @@ return {
     "lewis6991/gitsigns.nvim",
 
     config = function()
-        require("gitsigns").setup()
+        require("gitsigns").setup({
+            current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                delay = 200,
+                ignore_whitespace = true,
+                virt_text_priority = 100,
+            },
+
+            on_attach = function(bufnr)
+                local gitsigns = require('gitsigns')
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Blame line.
+                map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+            end,
+        })
+
         require("scrollbar.handlers.gitsigns").setup()
     end,
 }
