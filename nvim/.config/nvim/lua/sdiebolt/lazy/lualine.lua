29994d1fd5f236759790a1676eaf9b06ba0b7ed3
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-10-25 12:48:18 +0200

    feat(lualine): integrate noice.nvim and update sections
    - Added noice.nvim as a dependency for enhanced status line features.
    - Updated disabled filetypes to include 'neo-tree'.
    - Configured lualine sections to display noice.nvim status for command,
      mode, and search.
    - Added virtual environment display for Python files in lualine_y section.

--- nvim/.config/nvim/lua/sdiebolt/lazy/lualine.lua
+++ nvim/.config/nvim/lua/sdiebolt/lazy/lualine.lua
@@ -1,7 +1,44 @@
 return {
     "nvim-lualine/lualine.nvim",
 
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons", "folke/noice.nvim" },
 
-    opts = {disabled_filetypes = {"packer", "NVimTree"}},
+    opts = {
+        options = {
+            disabled_filetypes = { "packer", "NVimTree", "neo-tree" }
+        },
+        sections = {
+            lualine_a = { 'filename', 'mode' },
+            lualine_c = {
+                {
+                    require("noice").api.status.command.get,
+                    cond = require("noice").api.status.command.has,
+                    color = { fg = "#ff9e64" },
+                },
+            },
+            lualine_x = {
+                {
+                    require("noice").api.status.mode.get,
+                    cond = require("noice").api.status.mode.has,
+                    color = { fg = "#ff9e64" },
+                },
+                {
+                    require("noice").api.status.search.get,
+                    cond = require("noice").api.status.search.has,
+                    color = { fg = "#ff9e64" },
+                },
+                'encoding', 'fileformat', 'filetype',
+            },
+            lualine_y = {
+                {
+                    function()
+                        local venv = os.getenv("VIRTUAL_ENV_PROMPT") or os.getenv("VIRTUAL_ENV") or "NO ENV"
+                        return " " .. venv
+                    end,
+                    cond = function() return vim.bo.filetype == "python" end,
+                },
+                'progress',
+            },
+        }
+    }
 }
