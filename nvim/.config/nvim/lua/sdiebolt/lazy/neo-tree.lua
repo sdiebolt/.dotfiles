b2e51efe319c6486a2d5f1002fa556b161987ad5
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-10-29 10:07:02 +0100

    feat: move neo-tree to the right, add file follow

--- nvim/.config/nvim/lua/sdiebolt/lazy/neo-tree.lua
+++ nvim/.config/nvim/lua/sdiebolt/lazy/neo-tree.lua
@@ -11,7 +11,32 @@ return {
     },
 
     keys = {
-        {"<leader>nt" , "<Cmd>Neotree toggle<CR>", desc = "Neotree: toggle" },
-        {"<leader>np" , "<Cmd>Neotree position=current<CR>", desc = "Neotree: full"},
+        { "<leader>nt", "<Cmd>Neotree position=right toggle reveal<CR>",   desc = "Neotree: sidebar" },
+        { "<leader>np", "<Cmd>Neotree position=current toggle reveal<CR>", desc = "Neotree: full" },
     },
+
+    opts = {
+        buffers = {
+            follow_current_file = {
+                enabled = true,
+                leave_dirs_open = false,
+            },
+            group_empty_dirs = true,
+            show_unloaded = true,
+            window = {
+                mappings = {
+                    ["bd"] = "buffer_delete",
+                    ["<bs>"] = "navigate_up",
+                    ["."] = "set_root",
+                    ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+                    ["oc"] = { "order_by_created", nowait = false },
+                    ["od"] = { "order_by_diagnostics", nowait = false },
+                    ["om"] = { "order_by_modified", nowait = false },
+                    ["on"] = { "order_by_name", nowait = false },
+                    ["os"] = { "order_by_size", nowait = false },
+                    ["ot"] = { "order_by_type", nowait = false },
+                }
+            },
+        },
+    }
 }
