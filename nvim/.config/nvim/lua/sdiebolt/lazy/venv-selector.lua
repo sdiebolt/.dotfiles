264776df641bd623c4ae97f7cf7192d4798a6648
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-10-30 09:47:07 +0100

    feat(nvim): add venv-selector plugin

---
+++ nvim/.config/nvim/lua/sdiebolt/lazy/venv-selector.lua
@@ -0,0 +1,24 @@
+return {
+    "linux-cultist/venv-selector.nvim",
+
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "mfussenegger/nvim-dap",
+        "mfussenegger/nvim-dap-python",
+        {
+            "nvim-telescope/telescope.nvim",
+            branch = "0.1.x",
+            dependencies = { "nvim-lua/plenary.nvim" },
+        },
+    },
+
+    lazy = false,
+
+    branch = "regexp",
+
+    keys = {
+        { "<leader>vv", "<cmd>VenvSelect<cr>" },
+    },
+
+    opts = {},
+}
