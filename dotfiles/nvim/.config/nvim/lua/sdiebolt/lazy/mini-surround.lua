698bb7b5e01d3239f2a49b9adb7c2965d41757cb
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2025-01-09 00:27:44 +0100

    feat(nvim): add hardtime and mini-surround plugins
    Add hardtime.nvim to enforce better editing habits and mini.surround for
    easier text surrounding operations. Configure mini.surround with leader-based
    keymaps for better mnemonics.

---
+++ dotfiles/nvim/.config/nvim/lua/sdiebolt/lazy/mini-surround.lua
@@ -0,0 +1,20 @@
+return {
+    "echasnovski/mini.surround",
+
+    event = "VeryLazy",
+
+    version = "*",
+
+    config = {
+        mappings = {
+            add = "<leader>sa",       -- Add surroundng in Normal and Visual modes
+            delete = "<leader>sd",    -- Delete surrounding
+            find = "<leader>sf",      -- Find surrounding (to the right)
+            find_left = "<leader>sF", -- Find surrounding (to the left)
+            highlight = "<leader>sh", -- Highlight surrounding
+            replace = "<leader>sr",   -- Replace surrounding
+            update_n_lines = "",      -- Update surrounding for n lines
+        },
+        n_lines = 100,
+    },
+}
