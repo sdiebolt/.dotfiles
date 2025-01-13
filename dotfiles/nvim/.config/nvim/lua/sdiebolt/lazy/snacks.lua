43d0815278ae096abc9bb104e30778ddae035810
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2025-01-13 17:45:30 +0100

    fix(nvim): fix floating terminal binding

--- dotfiles/nvim/.config/nvim/lua/sdiebolt/lazy/snacks.lua
+++ dotfiles/nvim/.config/nvim/lua/sdiebolt/lazy/snacks.lua
@@ -60,6 +60,7 @@ return {
         { "<leader>gf", function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
         { "<leader>gl", function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
         { "<c-/>",      function() Snacks.terminal.toggle("zsh") end,    desc = "Toggle Terminal" },
+        { "<c-_>",      function() Snacks.terminal.toggle("zsh") end,    desc = "which_key_ignore" },
         { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference" },
         { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference" },
     },
