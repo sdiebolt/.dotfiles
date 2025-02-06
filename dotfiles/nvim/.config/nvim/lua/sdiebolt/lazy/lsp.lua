bacee2371477ff2731cc63013cd7faab20b0761d
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2025-02-06 13:33:39 +0100

    feat(nvim): add pin command for Typst

--- dotfiles/nvim/.config/nvim/lua/sdiebolt/lazy/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/sdiebolt/lazy/lsp.lua
@@ -30,6 +30,8 @@ return {
             lsp_zero.default_keymaps({ buffer = bufnr })
         end
 
+        local border = "single"
+
         lsp_zero.extend_lspconfig({
             capabilities = require("cmp_nvim_lsp").default_capabilities(),
             lsp_attach = lsp_attach,
@@ -50,9 +52,6 @@ return {
                 ["tinymist"] = { "typst" },
             }
         })
-
-
-        local border = "single"
 
         -- Add border to the diagnostic popup window
         vim.diagnostic.config({
@@ -148,8 +147,40 @@ return {
                         end,
                         settings = {
                             formatterMode = "typstyle",
+                            preview = {
+                                pinPreviewFalse = false,
+                            }
                         },
                     })
+
+                    vim.api.nvim_create_user_command("TypstPin", function()
+                        local tinymist_id = nil
+                        for _, client in pairs(vim.lsp.get_clients()) do
+                            if client.name == "tinymist" then
+                                tinymist_id = client.id
+                                break
+                            end
+                        end
+
+                        if not tinymist_id then
+                            vim.notify("Tinymist not running!", vim.log.levels.ERROR)
+                            return
+                        end
+
+                        local client = vim.lsp.get_client_by_id(tinymist_id)
+                        if client then
+                            client.request("workspace/executeCommand", {
+                                command = "tinymist.pinMain",
+                                arguments = { vim.api.nvim_buf_get_name(0) },
+                            }, function(err)
+                                if err then
+                                    vim.notify("Error pinning: " .. err, vim.log.levels.ERROR)
+                                else
+                                    vim.notify("Succesfully pinned!", vim.log.levels.INFO)
+                                end
+                            end, 0)
+                        end
+                    end, {})
                 end,
             },
         })
