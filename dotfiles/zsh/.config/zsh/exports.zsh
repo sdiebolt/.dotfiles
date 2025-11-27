4645d9792e720e03d145d8ab8a2e98659def1402
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2025-11-27 12:51:57 +0000

    feat(nvm): add nvm to Arch packages

--- dotfiles/zsh/.config/zsh/exports.zsh
+++ dotfiles/zsh/.config/zsh/exports.zsh
@@ -5,7 +5,6 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export PATH="$HOME/.local/bin":$PATH
-export PATH="$HOME/.local/share/zap":$PATH
 export TERM=xterm-256color
 
 # Fix pip SSL cert validation.
@@ -31,9 +30,7 @@ export FZF_CTRL_T_OPTS="
 export VIRTUALENVS="$HOME/.virtualenvs"
 
 # nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source /usr/share/nvm/init-nvm.sh
 
 # opencode
 export PATH=/home/sdiebolt/.opencode/bin:$PATH
